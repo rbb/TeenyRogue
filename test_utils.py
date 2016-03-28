5c65699bc1aa29116ebe7c980607d99eefd07306
Russell Brinkmann <rbb@users.noreply.github.com>
2016-03-28 02:13:35 -0600

    Added Monsters, Powerups, Ladder, Level, Status sprites
    TODO: make monsters move
    TODO: make powerups get "consumed" by player
    TODO: attacks
    TODO: use images (or textured surfaces) for the floors and the walls

---
+++ test_utils.py
@@ -0,0 +1,23 @@
+import numpy as np
+#import utils 
+from utils import *
+
+N = 100
+
+for n in range(N):
+    #m = utils.randint(n)
+    m = randint(n)
+    if m != 0 and m > n-1:
+        print ("m = " +str(m) +" n = " +str(n))
+    if m < 0:
+        print ("m = " +str(m) +" n = " +str(n))
+
+M = np.zeros(N)    
+for n in range(N):
+    m = randint(N)
+    if m != 0 and m > N-1:
+        print ("m = " +str(m) +" n = " +str(n))
+    if m < 0:
+        print ("m = " +str(m) +" n = " +str(n))
+    M[n] = m
+print ("mean = " +str(np.mean(M)) )
---
+++ utils.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+# -- Global constants
+VERBOSE = 1
+
+MAP_W = 10
+MAP_H = 10
+SCALE = 32
+SCREEN_W = SCALE*MAP_W
+SCREEN_H = SCALE*MAP_H
+STATUS_H = 48
+LEVEL_Y = SCREEN_H + STATUS_H/2 +1
+ 
+# Colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+BLUE = (50, 50, 255)
+RED = (255, 50, 50)
+PURPLE = (64, 0, 64)
+GRAY = (128, 128, 128)
+GRAY_25 = (64, 64, 64)
+GRAY_50 = (128, 128, 128)
+GRAY_75 = (192, 192, 192)
+GREENISH = (67, 232, 43)
+ 
+# Monster database
+#                                 hit          moves/ exp  stopped  ballistic
+#           Image name            pts, Damage, turn,  pts,  walls,    Damage,  Resurection
+M_DATA = [["images/bomba32.png",   2,    1,     1,    50,    False,    True ,     False ],    
+          ["images/ghost32.png",   2,    1,     1,    50,    True ,    False,     False ],
+          ["images/knight32.png",  2,    2,     1,    50,    False,    False,     False ],
+          ["images/rat32.png",     1,    1,     1,    50,    False,    False,     False ],
+          ["images/skull32.png",   2,    1,     1,    50,    False,    False,     True  ],
+          ["images/troll32.png",   2,    1,     1,    50,    False,    False,     False ] ]
+M_IMAGE_FNAME = 0
+M_HIT_PTS = 1
+M_DAMAGE = 2
+M_MOVES = 3             # Number of moves/attacks per turn
+M_EXP_PTS = 4           # Number of experience points earned for killing the monster
+M_WALL_STOP = 5         # True: Walls stop movement, False: monster goes through walls
+M_BALLISTIC = 6         # Attacks do ballistic damage
+
+# Equipment database
+#             Image Name          Damage, ballistic, global, targeting
+E_DATA = [["images/dagger32.png",    1,      True,    False,  False ],
+          ["images/firebomb32.png",  2,      True,    False,  False ],
+          ["images/firestorm32.png", 1,      False,   True,   False ],
+          ["images/freeze32.png",    1,      False,   True,   False ],
+          ["images/lightning32.png", 1,      False,   False,  True  ] ]
+E_DAGGER = 0
+E_FIRE_BOMB = 1
+E_FIRESTORM = 2
+#E_FREEZE_BOMB = 3
+E_FREEZE_STORM = 3
+E_LIGHTNING = 4
+# TODO other equipment
+ 
+def randint(maxval=1):
+    n_float = np.random.uniform(0, maxval-1)
+    n = int(round( n_float ))
+    #n = round( np.random.uniform(0, maxval-1) )
+    #if VERBOSE:
+    #    print ("randint.maxval = " +str(maxval) )
+    #    print ("randint.n_float = " +str(n_float) )
+    #    print ("randint.n = " +str(n) )
+    return n
