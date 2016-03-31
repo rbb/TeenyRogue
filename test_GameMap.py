53c915db8e7128a5785752403f8f5a72746d1811
Russell Brinkmann <rbb@users.noreply.github.com>
2016-03-30 22:24:43 -0600

    - Change GameMap.py to use y first in all numpy matrices, to match numpy's   internal structure.

--- test_GameMap.py
+++ test_GameMap.py
@@ -1,24 +1,27 @@
 import GameMap
 import numpy as np
 
-gm = GameMap.game_map()
-print( gm.m )
+gm = GameMap.game_map(5,10)
+gm.prn()
 print ("\n");
 
 print ("------- edgewalls() ----------");
+gm.setedges(0)
+gm.prn()
 gm.setedges(1)
-print( gm.m )
+gm.prn()
 print ("\n");
+
 
 print ("------- newrand() ----------");
 gm.newrand()
-print( gm.m )
+gm.prn()
 print( gm.m.mean() )
 print ("\n");
 
 print ("------- clearpath() ----------");
 gm.clearpath()
-print( gm.m )
+gm.prn()
 print( gm.m.mean() )
 print ("\n");
 
@@ -45,11 +48,37 @@ gm.m = np.array([ [ 1,1,1,1,1],
                   [ 1,1,1,1,1],
                   [ 1,0,1,0,1],
                   [ 1,1,1,1,1] ])
-print( gm.m )
+gm.prn()
 print( gm.m.mean() )
 print ("------- clearpath() ----------");
 gm.clearpath()
-print( gm.m )
+gm.prn()
 print( gm.m.mean() )
 print ("\n");
 print ("\n");
+
+print ("=========================================\n")
+gm = GameMap.game_map(5,6)
+gm.m = np.array([ [ 1,1,1,1,1],
+                  [ 1,0,1,0,1],
+                  [ 1,1,1,1,1],
+                  [ 1,0,1,0,1],
+                  [ 1,1,1,1,1],
+                  [ 1,1,1,1,1] ])
+gm.prn()
+print "shape = " +str(gm.m.shape)
+print "valid_path(1,1, 4,1) = " +str( gm.valid_path(1,1,4,1) )
+#print "the non-valid map shows up as " +str(gm.valid())
+print ("\n");
+
+gm = GameMap.game_map(5,5)
+gm.m = np.array([ [ 1,1,1,1,1],
+                  [ 1,0,1,0,1],
+                  [ 1,0,1,0,1],
+                  [ 1,0,0,0,1],
+                  [ 1,1,1,1,1] ])
+gm.prn()
+print "valid_path(1,1, 1,2) = " +str( gm.valid_path(1,1,1,2) )
+print "valid_path(1,1, 4,1) = " +str( gm.valid_path(1,1,4,1) )
+#print "the non-valid map shows up as " +str(gm.valid())
+
