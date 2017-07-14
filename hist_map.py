670e988a8c9e8640140f27f9c47b347ea9abe136
Test <Test>
2017-07-14 17:58:10 -0600

    Add logging of map generation attempts

---
+++ hist_map.py
@@ -0,0 +1,20 @@
+#import sys
+#import math
+#import pygame
+#from pygame.locals import *
+import numpy as np
+#import GameMap
+
+# This is a quick an dirty script to read the map_attempts.txt file, which has
+# data on the number of times a GameMap object had to be created in order to
+# get a valid map, and calculate some very basic statistcs on it.
+
+#with open("map_attempts.txt", "a") as myfile:
+#    myfile.write( str(n_map_attempts) +"\n" )
+
+d = np.genfromtxt('map_attempts.txt', delimiter=',')
+print "Map Generation attempts"
+print "Mean:    " +str( np.mean(d) )
+print "Std Dev: " +str( np.std(d) )
+
+
